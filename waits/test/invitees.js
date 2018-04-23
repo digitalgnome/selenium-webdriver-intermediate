9cdecf6486c8cda02d3d662172a60b3f2a60890e
Jay McGavren <jay@mcgavren.com>
2018-04-23 13:47:18 -0700

    s/var/let/

--- waits/test/invitees.js
+++ waits/test/invitees.js
@@ -2,12 +2,11 @@ const {Browser, By, Key, until} = require("selenium-webdriver");
 const {suite} = require("selenium-webdriver/testing");
 const assert = require('assert');
 
-var url = "https://treehouse-projects.github.io/rsvp/selenium-webdriver-wait/";
-// var url = "http://localhost:8000/";
+let url = "https://treehouse-projects.github.io/rsvp/selenium-webdriver-wait/";
 
 suite(function(env) {
     describe('RSVP site', function() {
-        var driver;
+        let driver;
 
         before(async function() {
             driver = await env.builder().build();
@@ -17,12 +16,12 @@ suite(function(env) {
             // Set AJAX delay to 3s then uncomment to demonstrate
             // this.timeout(5000);
             await driver.get(url);
-            var list = await driver.findElement(By.id('invitedList'));
+            let list = await driver.findElement(By.id('invitedList'));
             console.log(list.getTagName());
             await driver.wait(
                 until.elementLocated(By.css('#invitedList li'))
             );
-            var text = await list.getText();
+            let text = await list.getText();
             assert(text.includes("Craig Dennis"));
         });
 
