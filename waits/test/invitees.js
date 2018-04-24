f2fcc4b1fcd435ea558221c52b6c1d9f9ffad577
Jay McGavren <jay@mcgavren.com>
2018-04-23 17:23:14 -0700

    Add RsvpPage

--- waits/test/invitees.js
+++ waits/test/invitees.js
@@ -1,23 +1,23 @@
 const {Browser, By, Key, until} = require("selenium-webdriver");
 const {suite} = require("selenium-webdriver/testing");
 const assert = require('assert');
-
-let url = "https://treehouse-projects.github.io/rsvp/selenium-webdriver-wait/";
+const RsvpPage = require('../pages/rsvp.js');
 
 suite(function(env) {
     describe('RSVP site', function() {
         let driver;
+        let page;
 
         before(async function() {
             driver = await env.builder().build();
+            page = new RsvpPage(driver);
+            await page.open();
         });
 
         it('loads existing invitations', async function() {
             // Set AJAX delay to 3s then uncomment to demonstrate
             // this.timeout(5000);
-            await driver.get(url);
-            let list = await driver.findElement(By.id('invitedList'));
-            console.log(list.getTagName());
+            let list = await driver.findElement(page.locators.invitedList);
             await driver.wait(
                 until.elementLocated(By.css('#invitedList li'))
             );
